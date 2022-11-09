55c9167100490c2c97ba74f55e4dc80941fd726c
mazoukan <azouakanmed@gmail.com>
2022-11-09 21:38:16 +0100

    Adding snake and kebab examples

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@JsonTest
+class BeerDtoTest extends BaseTest{
+
+
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+
+    @Test
+    void testDeserialize() throws IOException {
+        String json = "{\"id\":\"64bfc852-7e58-454a-a4b0-b2ae199eb3b2\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2022-11-06T20:03:57.3552023+01:00\",\"lastUpdatedDate\":\"2022-11-06T20:03:57.3581968+01:00\"}";
+
+        BeerDto dto = objectMapper.readValue(json, BeerDto.class);
+
+        System.out.println(dto);
+
+    }
+}
